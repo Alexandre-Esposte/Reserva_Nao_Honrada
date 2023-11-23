c122e98afc0865c317f9ceac51e080eb26d7a7c0
Alexandre Esposte <alexandreesposte@gmail.com>
2023-11-23 15:36:01 -0300

    funcs

---
+++ utils/funcs.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import f1_score,recall_score,precision_score, confusion_matrix
+
+def curve(model,x_train,y_train,qte=100):
+
+    limiares = np.linspace(0,1,qte)
+
+    recalls = []
+    precisions =[]
+    f1s = []
+
+    for limiar in limiares:
+
+        
+        pred = model.predict_proba(x_train)[:,1]
+
+        y_pred = np.where(pred >= limiar,1,0)
+
+        recalls.append(recall_score(y_train,y_pred))
+        precisions.append(precision_score(y_train,y_pred))
+        f1s.append(f1_score(y_train,y_pred))
+
+
+    fig, ax = plt.subplots(1,2,figsize=(15,7))
+
+    ax[0].plot(limiares,precisions,label='Precision')
+    ax[0].plot(limiares,recalls,label='Recall')
+    ax[0].plot(limiares,f1s,label='F1')
+    ax[0].set_xlabel('Limiar')
+    ax[0].set_ylabel('(%)')
+    ax[0].legend()
+
+    ax[1].plot(recalls,precisions)
+    ax[1].set_xlabel('Recall')
+    ax[1].set_ylabel('Precision')
+
+    plt.tight_layout()
+    return limiares, recalls, precisions, f1s
+
+
+def viabilidade(model,x_test,y_test,limiar):
+
+    prob = model.predict_proba(x_test)[:,1]
+
+    pred = np.where(prob >= limiar,1,0)
+
+    matrix = confusion_matrix(y_test, pred)
+
+    vp = matrix[1,1]
+    vn = matrix[0,0]
+    fp = matrix[0,1]
+    fn = matrix[1,0]
